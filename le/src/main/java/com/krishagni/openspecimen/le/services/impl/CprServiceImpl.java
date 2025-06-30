34435784f707539d94043aca3d3e62760a25cc0e
Richard Bramley <rabramley@gmail.com>
2025-06-30 11:30:37 +0100

    Transactional

--- le/src/main/java/com/krishagni/openspecimen/le/services/impl/CprServiceImpl.java
+++ le/src/main/java/com/krishagni/openspecimen/le/services/impl/CprServiceImpl.java
@@ -25,6 +25,8 @@ import com.krishagni.catissueplus.core.common.service.LabelGenerator;
 import com.krishagni.openspecimen.le.events.BulkParticipantRegDetail;
 import com.krishagni.openspecimen.le.events.ParticipantRegDetail;
 import com.krishagni.openspecimen.le.services.CprService;
+import javax.transaction.Transactional
+
 
 public class CprServiceImpl implements CprService {
 	
